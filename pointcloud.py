3f6c1c7eea3b2a5594fae14e374cbb9ac46149c0
Federallo <aligiassioma72@gmail.com>
2024-08-14 10:15:08 +0200

    fixed some mistyping

--- pointcloud.py
+++ pointcloud.py
@@ -3,14 +3,14 @@ import open3d as o3d
 
 # generating pointcloud
 # for now pointclouds will be generated filtered and without noise
-def generatePointClouds(pointClouds, nSensors, i):
+def generatePointClouds(nSensors, i):
 
     pointclouds = dataset_loader.loadscan(nSensors, i, 0)
     
     pcds = []
     for pointcloud in pointclouds:
         pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(pointclouds)
+        pcd.points = o3d.utility.Vector3dVector(pointcloud)
         pcds.append(pcd)
     
     # combining pointcluds and removing duplicate coordinates
@@ -19,8 +19,8 @@ def generatePointClouds(pointClouds, nSensors, i):
     for pcd in pcds:
         for pt in pcd.points:
             if pt not in merged_pcd.points:
-                merged_pcd.points.append(point)
+                merged_pcd.points.append(pt)
 
     # returning filetered pointcloud
     filtered_pcd = merged_pcd.select_by_index([i for i in range(len(merged_pcd.points)) if merged_pcd.points[i][2] > 0.3])
-    return filtered_pcd.voxel_down_sampe(voxel_size = 0.25)
+    return filtered_pcd.voxel_down_sample(voxel_size = 0.25)
