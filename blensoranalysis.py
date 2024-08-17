11aa5dd8e43850d8a7d834d395f535e0b1829fe4
Federallo <aligiassioma72@gmail.com>
2024-08-17 12:01:28 +0200

    created a library that uses blensor_analysis repo to create clusters and bounding boxes

---
+++ blensoranalysis.py
@@ -0,0 +1,9 @@
+from _blensor_analysis import pointcloud as pc
+import pointcloud
+
+def generateBoundingBoxes(nSensors, i):
+    pointCloud = []
+    pointCloud.append(pointcloud.generatePointClouds(nSensors, i))
+    bounding_box = pc.clusterData(pointCloud)
+    bounding_box = bounding_box[0] # because we will display the bounding boxes of only one scan
+    return bounding_box
