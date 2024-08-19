1f9fadafe83921ce80fe0ae7a7cc28977ae35fbc
Federallo <aligiassioma72@gmail.com>
2024-08-19 14:10:18 +0200

    updated comments

--- blensoranalysis.py
+++ blensoranalysis.py
@@ -5,5 +5,5 @@ def generateBoundingBoxes(nSensors, i):
     pointCloud = []
     pointCloud.append(pointcloud.generatePointClouds(nSensors, i))
     bounding_box = pc.clusterData(pointCloud)
-    bounding_box = bounding_box[0] # because we will display the bounding boxes of only one scan
+    bounding_box = bounding_box[0] # because we will display the bounding boxes of only one scan, and blensor_analysis returns a list of bounding boxes
     return bounding_box
