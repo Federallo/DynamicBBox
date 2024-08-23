e8e4d75cfd02c0ea842ea74d7329b9f1f295d633
Federallo <aligiassioma72@gmail.com>
2024-08-23 17:25:11 +0200

    first implementation of points search close to the bounding box

--- customdbscan.py
+++ customdbscan.py
@@ -18,9 +18,13 @@ def pointsInBB(pointcloud, bounding_box):
     
     return points_in_box
 
-def customDBSCAN(pointcloud, bounding_box, eps, minPts):
+
+def customDBSCAN(pointcloud, bounding_box, eps, minPts, expanseFactor):
 
     points = pointsInBB(pointcloud, bounding_box) # getting the points that are within the bounding box   
+
+    pointcloud = np.asarray(pointcloud.points)
+
     if points.any(): # checking if there are any points in the bounding box
 
         labels = np.zeros(len(points)) -1 # initializing all points as noise (-1)
@@ -28,21 +32,30 @@ def customDBSCAN(pointcloud, bounding_box, eps, minPts):
 
         for i in range(len(points)):
             if labels[i] == -1:
-                
+
+                for j in range(len(pointcloud)):
+
+                    if pointcloud[j] not in points:  # Check if point is outside the bounding box
+
+                        # adding new pointcloud in case is close to the points inside the bounding box
+                        if np.linalg.norm(pointcloud[j]-points[i]) < expanseFactor: #(pointcloud[j] - nearestPoint(pointcloud[j], points))
+                            labels = np.concatenate((labels, [labels[i]])) 
+                            points = np.concatenate((points, [pointcloud[j]]))
+
                 neighbours = [j for j, point in enumerate(points) if np.linalg.norm(point - points[i]) < eps] # getting the neighbours of points[i] that are within eps distance
 
                 if len(neighbours) < minPts:
                     labels[i] = -1 # labeling point as noise because it doesn't have enough neighbours
                 else:
                     nCluster += 1
-                    labels = expandCluster(points, labels, i, neighbours, nCluster, eps, minPts)
+                    labels, points = expandCluster(points, labels, i, neighbours, nCluster, eps, minPts, pointcloud, expanseFactor)
 
         #returning new bounding box (/bounding boxes in case with the newly discovered clusters)
         return createBoundingBoxes(labels, points)
     else:
         return None, None
 
-def expandCluster(points, labels, i, neighbours, nCluster, eps, minPts):
+def expandCluster(points, labels, i, neighbours, nCluster, eps, minPts, pointcloud, expanseFactor):
 
     # assing the current point to a cluster
     labels[i] = nCluster
@@ -51,11 +64,23 @@ def expandCluster(points, labels, i, neighbours, nCluster, eps, minPts):
         #checking if the current point is labeled as noise
         if labels[j] == -1:
             labels[j] = nCluster
+        
+            for l in range(len(pointcloud)):
+
+                if pointcloud[l] not in points:  # Check if point is outside the bounding box
+
+                        # adding new pointcloud in case is close to the points inside the bounding box
+                        if np.linalg.norm(pointcloud[l]-points[j]) < expanseFactor: #(pointcloud[j] - nearestPoint(pointcloud[j], points))
+                            labels = np.concatenate((labels, [labels[j]])) 
+                            points = np.concatenate((points, [pointcloud[l]]))
+
+
             #seraching for new neighbours to add to "neighbours" variable
             newNeighbours = [k for k, point in enumerate(points) if np.linalg.norm(point - points[j]) < eps]
             if len(newNeighbours) >= minPts:
                 neighbours += newNeighbours
-    return labels
+
+    return labels, points
 
 # considering only the bigger cluster
 def createBoundingBoxes(labels, points):
@@ -69,7 +94,7 @@ def createBoundingBoxes(labels, points):
 
     if len(effectiveLabels) != 0:
 
-        uniqueClusterLabels, countClusterLabels = np.unique(labels, return_counts=True) # getting the unique cluster labels and their counts
+        uniqueClusterLabels, countClusterLabels = np.unique(effectiveLabels, return_counts=True) # getting the unique cluster labels and their counts
         mostFrequentClusterLabel = uniqueClusterLabels[np.argmax(countClusterLabels)] # getting the most frequent cluster label
 
         clusterPoints = o3d.geometry.PointCloud()
