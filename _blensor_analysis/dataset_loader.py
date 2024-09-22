8ca6250795b204cf14fecf137ca35c88c99294ff
Federallo <aligiassioma72@gmail.com>
2024-09-22 12:34:51 +0200

    added blensor_analysis' dependencies

---
+++ _blensor_analysis/dataset_loader.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def loadscan(sensors,scan, noisy):
+        
+        pos= np.loadtxt("./_blensor_analysis/BLENSOR/pitt_sensor_positions.csv", delimiter=",",skiprows=1,usecols=range(0,3))
+    
+        points=[]
+        path=str(scan)
+        for i in range (0,sensors):
+                data11= np.loadtxt("./_blensor_analysis/BLENSOR/sensor_"+str(i)+"_"+path+".csv", delimiter=",",skiprows=1)
+                
+                a=pos[i,0]
+                b=pos[i,1]
+                c=pos[i,2]
+
+                if(noisy==1):
+                        data1=[]
+                        
+                        
+                        addx=np.full_like(data11[:,8],a)
+                        xcoords=data11[:,8]+addx
+                        data1.append(xcoords)
+                        
+                        
+                        addy=np.full_like(data11[:,9],b)
+                        ycoords=data11[:,9]+addy
+                        data1.append(ycoords)
+                        
+                        
+                        addz=np.full_like(data11[:,10],c)
+                        zcoords=data11[:,10]+addz
+                        data1.append(zcoords)
+                        
+
+                else:
+                        data1=[]
+
+                        addx=np.full_like(data11[:,5],a)
+                        xcoords=data11[:,5]+addx
+                        data1.append(xcoords)
+                        
+
+                        addy=np.full_like(data11[:,6],b)
+                        ycoords=data11[:,6]+addy
+                        data1.append(ycoords)
+                        
+                        addz=np.full_like(data11[:,7],c)
+                        zcoords=data11[:,7]+addz
+                        data1.append(zcoords)
+                        
+
+                points1 = np.vstack(data1).T
+                points.append(points1)
+
+        return points
+
+def loadpitt():
+
+        trajectories= np.loadtxt("./_blensor_analysis/BLENSOR/pitt_trajectories.csv", delimiter=",",skiprows=1,usecols=range(0,3))
+        x_coords=trajectories[:,1]
+        y_coords=trajectories[:,2]
+        plt.plot(x_coords, y_coords, marker='o', linestyle='',markersize=3)  
+        plt.xlabel("X-axis")
+        plt.ylabel("Y-axis")
+        plt.title("Real Trajectories")
+        plt.show()
+                
---
+++ _blensor_analysis/pointcloud.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib as plt
+import open3d as o3d
+
+def generateGraphics(pointclouds,filtered):
+
+  pcds=[]
+  for pointcloud in pointclouds:
+    pcd= o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(pointcloud)
+    pcds.append(pcd)
+  
+  combined_pcd = o3d.geometry.PointCloud()
+
+  for pcd in pcds:
+    for point in pcd.points:
+      if point not in combined_pcd.points:
+        combined_pcd.points.append(point)
+  
+  if(filtered==1):
+    filtered_pcd = combined_pcd.select_by_index([i for i in range(len(combined_pcd.points)) if combined_pcd.points[i][2] > 0.3])
+    filtered_downsampled_pcd=filtered_pcd.voxel_down_sample(voxel_size=0.25)
+    return filtered_downsampled_pcd
+  
+  else:
+    combined_downsampled_pcd=combined_pcd.voxel_down_sample(voxel_size=0.25)
+    return combined_downsampled_pcd
+  
+
+def clusterData(pcds):
+  global_bboxes=[]
+  
+  for pcd in pcds:
+
+    labels = np.array(pcd.cluster_dbscan(eps=1.5, min_points=5, print_progress=True))
+
+    clusters = []
+    for label in np.unique(labels):
+        cluster_mask = (labels == label)
+        clusters.append(pcd.select_by_index(np.where(cluster_mask)[0]))
+
+    bounding_boxes = []
+    for cluster in clusters:
+        aabb = cluster.get_oriented_bounding_box()
+        aabb.color = [0,1,0]
+        bounding_boxes.append(aabb)
+        #global_bboxes.append(aabb)
+    global_bboxes.append(bounding_boxes)
+
+    colors = plt.cm.tab10(labels / (labels.max() if labels.max() > 0 else 1))
+    pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
+    
+    o3d.visualization.draw([pcd,*bounding_boxes],show_skybox=False)
+    
+  return global_bboxes
+
+def visualizeGraphics(pcds):
+  for pcd in pcds:
+    o3d.visualization.draw([pcd],show_skybox=False)
+    
+  
